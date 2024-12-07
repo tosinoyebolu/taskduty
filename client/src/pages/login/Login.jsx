c60d048d8c147fa41da54e0b525c1ead5741a01c
Oluwatosin Oyebolu <tosinoyebolu@gmail.com>
2024-12-07 15:07:38 +0100

    fixes client

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,22 +1,16 @@
-import { Container } from "react-bootstrap";
-import { Form } from "react-router-dom";
+import { Container, Form } from "react-bootstrap";
 
 export default function Login() {
   return (
     <Container fluid="xl" className="p-4">
-<div className="d-flex flex-colum justify-content-center align-items-center min-vh-100">
-<Form>
-
-<Form.Group className="mb-3" controlId="username">
-    <Form.label>Username </Form.label>
-    <Form.control 
-    type="text 
-    placeholder="username"
-    size="lg"
-    />
-</Form.Group>
-</Form>
-</div>
-</Container>
-);
+      <div className="d-flex flex-colum justify-content-center align-items-center min-vh-100">
+        <Form>
+          <Form.Group className="mb-3" controlId="username">
+            <Form.Label control="username">Username </Form.Label>
+            <Form.Control type="text " placeholder="username" size="lg" />
+          </Form.Group>
+        </Form>
+      </div>
+    </Container>
+  );
 }
